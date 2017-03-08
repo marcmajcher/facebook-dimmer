efa4faac494125a9c39d7fbe1ad60564321b9de5
Marc Majcher <majcher@gmail.com>
2017-03-07 21:16:22 -0600

    working timer for 60-second window

--- dimmer.js
+++ dimmer.js
@@ -1,17 +1,46 @@
 /* eslint-env jquery, browser */
+/* global TimeMe */
 
 (function() {
   'use strict';
 
-  var ratio = 1;
+  TimeMe.initialize({
+    currentPageName: 'facebook-dimmer',
+    idleTimeoutInSeconds: 300
+  });
+  TimeMe.startTimer();
+
+  var lastTime = 0;
+  var timeWindow = 60;
+  var seconds = Array(timeWindow + 1).join('0');
 
   function fade() {
-    if (ratio > 0) {
-      ratio -= 0.01;
+    var time = TimeMe.getTimeOnCurrentPageInSeconds();
+    if (seconds.length >= timeWindow) {
+      seconds = seconds.slice(1);
     }
+    if (time === lastTime) {
+      seconds += '0';
+    }
+    else {
+      seconds += '1';
+    }
+    lastTime = time;
 
-    $('body').css('opacity', ratio);
+    var idleSeconds = seconds.split('0').length - 1;
+    console.log('Seconds idle in last minute:', idleSeconds);
+
+    document.querySelector('body').style.opacity = idleSeconds / timeWindow;
   }
 
-  setInterval(fade, 100);
+  setInterval(fade, 1000);
 })();
+
+// https://developer.chrome.com/extensions/storage#type-StorageArea
+// chrome.storage.sync.set({ "yourBody": "myBody" }, function(){
+//     //  A data saved callback omg so fancy
+// });
+//
+// chrome.storage.sync.get(/* String or Array */["yourBody"], function(items){
+//     //  items = [ { "yourBody": "myBody" } ]
+// });
