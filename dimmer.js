37390679cd837e49c21429974de089e2dc6a3e5f
Marc Majcher <majcher@gmail.com>
2017-03-07 23:31:46 -0600

    added 4/1 time window, switched to minutes

--- dimmer.js
+++ dimmer.js
@@ -5,49 +5,54 @@
   'use strict';
 
   var lastTime = 0;
-  var timeWindow = 15;
-  var seconds = Array(timeWindow + 1).join('0');
+  var maxActive = 60; // Set to only allow sixty minutes of facebook time
+  var timeWindow = 60 * 4; // every four hours
+  var idleString = Array(timeWindow + 1).join('0');
+  var delay = 60 * 1000; // minutes
 
   window.onload = function() {
     TimeMe.initialize({
       currentPageName: 'facebook-dimmer',
-      idleTimeoutInSeconds: 300
+      idleTimeoutInSeconds: 60
     });
     TimeMe.startTimer();
 
     chrome.storage.sync.get(null, function(items) {
-      if (items.idleSeconds) {
-        seconds = items.idleSeconds;
-        var secondsGone = Math.floor((new Date().getTime() - items.lastTime) / 1000);
-        seconds += Array(secondsGone + 1).join('0');
-        seconds = seconds.slice(-timeWindow);
+      if (items.idleTime) {
+        idleString = items.idleTime;
+        var timeAway = Math.floor((new Date().getTime() - items.lastTime) / 1000);
+        idleString += Array(timeAway + 1).join('0');
+        idleString = idleString.slice(-timeWindow);
         setOpacity();
-        setInterval(fade, 1000);
       }
+      setInterval(fade, delay);
     });
   };
 
   function setOpacity() {
-    document.querySelector('body').style.opacity = (seconds.split('0').length - 1) / timeWindow;
+    var idleTime = Math.max(0, maxActive - (idleString.split('1').length - 1));
+    document.querySelector('body').style.opacity = idleTime / maxActive;
   }
 
   function fade() {
     var time = TimeMe.getTimeOnCurrentPageInSeconds();
-    if (seconds.length >= timeWindow) {
-      seconds = seconds.slice(1);
+    if (idleString.length >= timeWindow) {
+      idleString = idleString.slice(1);
     }
     if (time === lastTime) {
-      seconds += '0';
+      idleString += '0';
     }
     else {
-      seconds += '1';
+      idleString += '1';
     }
     lastTime = time;
 
     chrome.storage.sync.set({
-      idleSeconds: seconds,
+      idleTime: idleString,
       lastTime: new Date().getTime()
     });
+    // chrome.storage.sync.clear();
+
     setOpacity();
   }
 })();
